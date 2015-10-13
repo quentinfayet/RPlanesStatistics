af14764437e76d0bae7f001184dda61e7f1c9e4c
quentin.fayet <admin@example.com>
2015-10-13 08:26:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Planes statistics graphs with R
+
+## Dataset
+
+The dataset I used describes planes activity in 2006, and can be downloaded [here (bz2 archive)](http://stat-computing.org/dataexpo/2009/2006.csv.bz2).
---
+++ planes.R
@@ -0,0 +1,56 @@
+library("ggplot2"); #Loading graphical library
+
+
+# ======= Loading the dataset as data frame
+
+cat("Loading dataset.\n");
+
+planes <- read.delim("2006.csv",
+                     sep = ",",
+                     stringsAsFactors = FALSE,
+                     header = TRUE,
+                     na.string = "");
+
+cat("Dataset loaded.\n");
+
+
+# ======== Organizing dataset
+
+# Vector of columns name to be kepts
+columns.toKeep <- c("Year", "Month", "DayofMonth", "DepTime", "CRSDepTime", "ArrTime", "CRSArrTime", "ActualElapsedTime",
+"CRSElapsedTime", "AirTime", "ArrDelay", "DepDelay", "Origin", "Dest", "TaxiIn", "TaxiOut", "Cancelled");
+
+# Keeping only columns from columns.toKeep
+cat("Removing useless columns.\n");
+planes <- planes[,columns.toKeep];
+cat("Columns removed.\n");
+
+# Building a Date object "Date" column
+cat("Creating \"Date\" column from Year, Month, DayofMonth.\n");
+planes <- transform(planes,
+                    Date = as.Date(paste(Year,
+                                         Month,
+                                         DayofMonth,
+                                         sep = "/"),
+                                   "%Y/%m/%d"));
+cat("\"Date\" column created.\n");
+
+# Building a YearMonth string from Date column
+cat("Creating \"YearMonth\" column.\n");
+planes$YearMonth <- strftime(planes$Date, format = "%Y/%m");
+cat("\"YearMonth\" column created.\n");
+
+print(head(planes));
+
+# ========== Printing graph: How many flight / months
+# Graph
+graph.howManyPlanes = ggplot(planes, aes=(x=YearMonth)) + geom_histogram();
+
+# Saving graph
+#ggsave(plot=graph.howManyPlanes,
+#       fileName = "flightsPerMonth.png",
+#       height = 6,
+#       width = 8);
+
+# Printing graph
+print(graph.howManyPlanes);
